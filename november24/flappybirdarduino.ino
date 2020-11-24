d333fb9b304f855700e9efb7e00fffe1017707ce
Katie Ferreol <kmf527@nyu.edu>
2020-11-24 17:12:22 +0400

    Update flappybirdarduino.ino

--- november24/flappybirdarduino.ino
+++ november24/flappybirdarduino.ino
@@ -1,48 +1,35 @@
-/*
-  Melody
-
-  Plays a melody
-
-  circuit:
-  - 8 ohm speaker on digital pin 8
-
-  created 21 Jan 2010
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Tone
-*/
 
 #include "pitches.h"
 
-char val; // Data received from the serial port
-int speakerPin = 8; // Set the pin to digital I/O 13
+//setting up variables
+char val;
+int speakerPin = 8;
 const int potentiometerPin = A0;
 const int photoresistorPin = A1;
 
 
-// notes in the melody:
+// 1-UP sound
 int melody[] = {
   NOTE_E6, NOTE_G6, NOTE_E7, NOTE_C7, NOTE_D7, NOTE_G7,
 };
 
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
+// 1-UP tempo
 int noteDurations[] = {
   8, 8, 8, 8, 8, 8,
 };
 
+// introduction sound
 int intro_melody[] = {
-  NOTE_B4, NOTE_B5, NOTE_FS5, NOTE_DS5, //1
+  NOTE_B4, NOTE_B5, NOTE_FS5, NOTE_DS5,
   NOTE_B5, NOTE_FS5, NOTE_DS5, NOTE_C5,
   NOTE_C6, NOTE_G5, NOTE_E5, NOTE_C6, NOTE_G5, NOTE_E5,
 
-  NOTE_B4,  NOTE_B5,  NOTE_FS5,   NOTE_DS5,  NOTE_B5,  //2
+  NOTE_B4,  NOTE_B5,  NOTE_FS5,   NOTE_DS5,  NOTE_B5,
   NOTE_FS5, NOTE_DS5,  NOTE_DS5, NOTE_E5,  NOTE_F5,
   NOTE_F5,  NOTE_FS5,  NOTE_G5,  NOTE_G5, NOTE_GS5,  NOTE_A5, NOTE_B5,
 };
 
+// introduction tempo
 int intro_tempo[] = {
   8, 8, 8, 8,
   8, 8, 8, 8,
@@ -54,59 +41,46 @@ int intro_tempo[] = {
 };
 
 void setup() {
-  pinMode(speakerPin, OUTPUT); // Set pin as OUTPUT
-  Serial.begin(9600); // Start serial communication at 9600 bps
+  pinMode(speakerPin, OUTPUT);
+  Serial.begin(9600);
   establishContact();
 
+  //intro playing once when Processing loads
   int size = sizeof(intro_melody) / sizeof(int);
   for (int thisNote = 0; thisNote < size; thisNote++) {
-
-    // to calculate the note duration, take one second divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / intro_tempo[thisNote];
     tone(speakerPin, intro_melody[thisNote], noteDuration);
-
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
     noTone(speakerPin);
-    if (thisNote == size-1) {
-      Serial.println("S");
-    }
   }
 }
 
 void loop() {
   int sensorValue1 = analogRead (potentiometerPin);
-  byte switchBG = map(sensorValue1, 0, 1023, 0, 3);
+  byte switchBG = map(sensorValue1, 0, 1023, 0, 4);
   Serial.println(switchBG);
 
-  if (Serial.available() > 0) { // If data is available to read,
+  if (Serial.available() > 0) {
     val = Serial.read();
 
-    if (val == '4')
-    { // If 1 was received
+    //if bird is in the slot between the pipes, play 1-UP sound
+    if (val == '4') {
       for (int thisNote = 0; thisNote < 6; thisNote++) {
-
-        // to calculate the note duration, take one second divided by the note type.
-        //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
         int noteDuration = 1000 / noteDurations[thisNote];
         tone(speakerPin, melody[thisNote], noteDuration);
-
-        // to distinguish the notes, set a minimum time between them.
-        // the note's duration + 30% seems to work well:
         int pauseBetweenNotes = noteDuration * 1.30;
         delay(pauseBetweenNotes);
-        // stop the tone playing:
       }
+      //if bird is not in the slot between the pipes, don't play 1-UP sound
     } else if (val == '5') {
       noTone(speakerPin);
     }
   }
 }
 
+//establish contact between Arduino and Processing
 void establishContact() {
   while (Serial.available() <= 0) {
     Serial.println("A");   // send a capital A
